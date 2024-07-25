efc4e5eaffbff3e337a96e680a7208932f311994
ashferoz <ash.safrina@gmail.com>
2024-07-26 00:08:08 +0800

    User can log in.

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import Navbar from "./components/Navbar";
 import { Suspense, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
+import UserContext from './contexts/user'
 const queryClient = new QueryClient();
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <UserContext.Provider value={{ accessToken, setAccessToken }}>
         <div className="font-fraunces">
           <Navbar />
           <Suspense fallback={<p>Loading...</p>}>
@@ -21,9 +25,11 @@ function App() {
               <Route path="/main" element={<Main />} />
               <Route path="/profile" element={<Profile />}/>
               <Route path="/inbox" />
+              <Route path="/login" element={<Login />}/>
             </Routes>
           </Suspense>
         </div>
+        </UserContext.Provider>
       </QueryClientProvider>
     </>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import useFetch from '../hooks/useFetch';
+import userContext from '../contexts/user'
+import { useQuery } from '@tanstack/react-query';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const usingFetch = useFetch()
+    const userCtx = useContext(userContext)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+
+    const { isError, error, data, refetch } = useQuery({
+        queryKey: ["login"],
+        queryFn: async () => {
+          try {
+            return await usingFetch("/auth/login", "POST", { username, hashed_password: password });
+          } catch (error) {
+            throw error.message;
+          }
+        }, 
+        enabled: false,
+      });
+    
+      useEffect(() => {
+        if (data) {
+          userCtx.setAccessToken(data.access);
+          const decoded = jwtDecode(data.access);
+        //   userCtx.setRole(decoded.role);
+        navigate('/main');
+        }
+      }, [data]);
+      
     return (
         <div className="flex items-center justify-center text-center min-h-screen bg-[#ffc0cc] font-epilogue">
             <div className="flex flex-col w-96">
             <h1 className="text-3xl font-bold font-fraunces mb-5">Login</h1>
-            <input type="text" placeholder="Email" className=" h-10 pl-3 mb-3" />
-            <input type="password" placeholder="Password" className=" h-10 pl-3 mb-5" />
-            <button className="bg-[#0753d8] text-white w-auto text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out">
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className=" h-10 pl-3 mb-3" />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className=" h-10 pl-3 mb-5" />
+            <button onClick={refetch} className="bg-[#0753d8] text-white w-auto text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out">
           Login
         </button>
             </div>

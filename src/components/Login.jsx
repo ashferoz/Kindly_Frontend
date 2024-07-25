dccffa9f5436e4c26aca78ba79b6ce2bf388a0e8
ashferoz <ash.safrina@gmail.com>
2024-07-25 15:51:43 +0800

    Adjust input type to password.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
             <div className="flex flex-col w-96">
             <h1 className="text-3xl font-bold font-fraunces mb-5">Login</h1>
             <input type="text" placeholder="Email" className=" h-10 pl-3 mb-3" />
-            <input type="text" placeholder="Password" className=" h-10 pl-3 mb-5" />
+            <input type="password" placeholder="Password" className=" h-10 pl-3 mb-5" />
             <button className="bg-[#0753d8] text-white w-auto text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out">
           Login
         </button>

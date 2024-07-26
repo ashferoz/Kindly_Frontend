8bf6bd41ab0a47b32dcd9eca7326fb0c91854016
ashferoz <ash.safrina@gmail.com>
2024-07-26 11:52:43 +0800

    User can register and login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,74 @@
-import React, { useContext, useEffect, useState } from 'react';
-import useFetch from '../hooks/useFetch';
-import userContext from '../contexts/user'
-import { useQuery } from '@tanstack/react-query';
-import { jwtDecode } from 'jwt-decode';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+import userContext from "../contexts/user";
+import { useQuery } from "@tanstack/react-query";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const usingFetch = useFetch()
-    const userCtx = useContext(userContext)
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate()
+  const usingFetch = useFetch();
+  const userCtx = useContext(userContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const { isError, error, data, refetch } = useQuery({
-        queryKey: ["login"],
-        queryFn: async () => {
-          try {
-            return await usingFetch("/auth/login", "POST", { username, hashed_password: password });
-          } catch (error) {
-            throw error.message;
-          }
-        }, 
-        enabled: false,
-      });
-    
-      useEffect(() => {
-        if (data) {
-          userCtx.setAccessToken(data.access);
-          const decoded = jwtDecode(data.access);
-        //   userCtx.setRole(decoded.role);
-        navigate('/profile');
-        }
-      }, [data]);
-      
-    return (
-        <div className="flex items-center justify-center text-center min-h-screen bg-[#ffc0cc] font-epilogue">
-            <div className="flex flex-col w-96">
-            <h1 className="text-3xl font-bold font-fraunces mb-5">Login</h1>
-            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className=" h-10 pl-3 mb-3" />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className=" h-10 pl-3 mb-5" />
-            <button onClick={refetch} className="bg-[#0753d8] text-white w-auto text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out">
+  const { isError, error, data, refetch } = useQuery({
+    queryKey: ["login"],
+    queryFn: async () => {
+      try {
+        return await usingFetch("/auth/login", "POST", {
+          username,
+          hashed_password: password,
+        });
+      } catch (error) {
+        throw error.message;
+      }
+    },
+    enabled: false,
+  });
+
+  useEffect(() => {
+    if (data) {
+      userCtx.setAccessToken(data.access);
+      const decoded = jwtDecode(data.access);
+      //   userCtx.setRole(decoded.role);
+      navigate("/profile");
+    }
+  }, [data]);
+
+  return (
+    <div className="flex items-center justify-center text-center min-h-screen bg-[#ffc0cc] font-epilogue">
+      <div className="flex flex-col w-96">
+        <h1 className="text-3xl font-bold font-fraunces mb-5">Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className=" h-10 pl-3 mb-3"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className=" h-10 pl-3 mb-5"
+        />
+        <button
+          onClick={refetch}
+          className="bg-[#0753d8] text-white w-auto text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out mb-5"
+        >
           Login
         </button>
-            </div>
-        </div>
-    );
+        <p className="font-light">
+          Don't have an account yet?{" "}
+          <a className="italic font-normal underline" href="/register">
+            Register with us!
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import FormBtn from "../components/FormBtn";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useFetch from "../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [selectedRole, setSelectedRole] = useState("");
+  const [bio, setBio] = useState("");
+  const [location, setLocation] = useState("");
+  const usingFetch = useFetch();
+  const queryClient = useQueryClient()
+
+  const navigate = useNavigate()
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      usingFetch(
+        "/auth/register",
+        "PUT",
+        {
+          firstname,
+          lastname,
+          email,
+          username,
+          hashed_password: password,
+          role_id: selectedRole,
+          bio,
+          location_id: location,
+        },
+        undefined
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["users"])
+      navigate('/login')
+    },
+  });
+
   return (
     <div className="flex items-center justify-center text-center min-h-screen bg-[#ffc0cc] font-epilogue">
-      <div className="flex flex-col">
+      <div className="flex flex-col mb-10">
         <h1 className="text-3xl font-bold font-fraunces mb-3">Register</h1>
         <h2 className="text-sm mb-5">
           Please provide all required details to register with us.
@@ -13,17 +53,48 @@ const Register = () => {
           Name<span className="text-[#eb5353]">&#42;</span>
         </p>
         <div className="flex gap-5 mx-5 mb-5">
-          <input type="text" placeholder="First" className="w-60 h-10 pl-3" />
-          <input type="text" placeholder="Last" className="w-60 h-10 pl-3" />
+          <input
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+            type="text"
+            placeholder="First"
+            className="w-60 h-10 pl-3"
+          />
+          <input
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            type="text"
+            placeholder="Last"
+            className="w-60 h-10 pl-3"
+          />
         </div>
         <p className="text-left mx-5 mb-1">
           Email<span className="text-[#eb5353]">&#42;</span>
         </p>
-        <input className="w-auto mx-5 mb-5 h-10 pl-3" type="text" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-auto mx-5 mb-5 h-10 pl-3"
+          type="email"
+        />
+        <p className="text-left mx-5 mb-1">
+          Username<span className="text-[#eb5353]">&#42;</span>
+        </p>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="w-auto mx-5 mb-5 h-10 pl-3"
+          type="text"
+        />
         <p className="text-left mx-5 mb-1">
           Password<span className="text-[#eb5353]">&#42;</span>
         </p>
-        <input className="w-auto mx-5 mb-5 h-10 pl-3" type="password" />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-auto mx-5 mb-5 h-10 pl-3"
+          type="password"
+        />
         <p className="text-left mx-5 mb-1">
           Role<span className="text-[#eb5353]">&#42;</span>
         </p>
@@ -34,8 +105,9 @@ const Register = () => {
                 type="radio"
                 id="volunteer"
                 name="role"
-                value="volunteer"
-                checked
+                value="VOLUNTEER"
+                checked={selectedRole === "VOLUNTEER"}
+                onChange={(e) => setSelectedRole(e.target.value)}
               />
               <label className="ml-2" htmlFor="volunteer">
                 Volunteer
@@ -46,7 +118,9 @@ const Register = () => {
                 type="radio"
                 id="beneficiary"
                 name="role"
-                value="beneficiary"
+                value="BENEFICIARY"
+                checked={selectedRole === "BENEFICIARY"}
+                onChange={(e) => setSelectedRole(e.target.value)}
               />
               <label className="ml-2" htmlFor="beneficiary">
                 Beneficiary
@@ -55,12 +129,28 @@ const Register = () => {
           </div>
         </div>
         <p className="text-left mx-5 mb-1">Bio</p>
-        <input className="w-auto mx-5 mb-5 h-10 pl-3" type="text" />
+        <input
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+          className="w-auto mx-5 mb-5 h-10 pl-3"
+          type="text"
+        />
         <p className="text-left mx-5 mb-1">Location</p>
-        <input className="w-auto mx-5 mb-5 h-10 pl-3" type="text" />
-        <button className="bg-[#0753d8] text-white w-auto mx-5 text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out">
+        <input
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          className="w-auto mx-5 mb-5 h-10 pl-3"
+          type="text"
+        />
+        <button
+          onClick={mutate}
+          className="bg-[#0753d8] text-white w-auto mx-5 text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out mb-8"
+        >
           Register
         </button>
+        <a className="italic font-normal underline" href="/login">
+          Already have an account?
+        </a>
       </div>
     </div>
   );

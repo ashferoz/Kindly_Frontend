922fbd86bd91e3089b5a5207916b17c4f4078e77
ashferoz <ash.safrina@gmail.com>
2024-07-30 17:59:04 +0800

    Update css on requestcard.

--- src/components/RequestCard.jsx
+++ src/components/RequestCard.jsx
@@ -4,7 +4,7 @@ const RequestCard = (props) => {
   return (
     <>
     <div
-  className="relative w-[350px] h-[250px] bg-[#9dc3c2] text-[#352a1f] flex flex-col justify-end hover:scale-105 transition-transform duration-200 ease-in-out cursor-pointer"
+  className="relative w-[350px] h-[350px] bg-[#9dc3c2] text-[#352a1f] flex flex-col justify-end hover:scale-105 transition-transform duration-200 ease-in-out cursor-pointer"
   onClick={props.onClick}
 >
   <div className="absolute top-0 left-0 w-full h-[75%] overflow-hidden">
@@ -14,7 +14,7 @@ const RequestCard = (props) => {
       alt=""
     />
   </div>
-  <div className="relative flex flex-col justify-center px-2 mb-3 h-[25%] z-10 bg-[#9dc3c2]">
+  <div className="relative flex flex-col justify-center py-1 px-2 h-auto z-10 bg-[#9dc3c2]">
     <div className="flex flex-col items-start text-left">
       <h4 className="text-md font-epilogue">{props.username}</h4>
       <h4 className="text-xl font-semibold">{props.title}</h4>

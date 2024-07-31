0b1f610cb3c9ceeb5aa314184b7ec3a5b2ce9985
ashferoz <ash.safrina@gmail.com>
2024-07-31 22:19:33 +0800

    Add register validations.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,7 +11,14 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [selectedRole, setSelectedRole] = useState("");
   const [bio, setBio] = useState("");
-  const [location, setLocation] = useState("");
+  const [errors, setErrors] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    username: "",
+    password: "",
+    selectedRole: "",
+  });
   const usingFetch = useFetch();
   const queryClient = useQueryClient();
 
@@ -40,6 +47,62 @@ const Register = () => {
     },
   });
 
+  const validate = () => {
+    const newErrors = { ...errors };
+    let isValid = true;
+
+    if (!firstname) {
+      newErrors.firstname = "First name is required";
+      isValid = false;
+    } else {
+      newErrors.firstname = "";
+    }
+
+    if (!lastname) {
+      newErrors.lastname = "Last name is required";
+      isValid = false;
+    } else {
+      newErrors.lastname = "";
+    }
+
+    if (!email.includes('@')) {
+      newErrors.email = "Email needs to be valid";
+      isValid = false;
+    } else {
+      newErrors.email = "";
+    }
+
+    if (!username) {
+      newErrors.username = "Username is required";
+      isValid = false;
+    } else {
+      newErrors.username = "";
+    }
+
+    if (password.length < 8) {
+      newErrors.password = "Password needs to be longer than 8 characters";
+      isValid = false;
+    } else {
+      newErrors.password = "";
+    }
+
+    if (!selectedRole) {
+      newErrors.selectedRole = "Role is required";
+      isValid = false;
+    } else {
+      newErrors.selectedRole = "";
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmitBtn = () => {
+    if (validate()) {
+      mutate();
+    }
+  };
+
   return (
     <div className="flex items-center justify-center text-center min-h-screen pt-20 bg-[#ffdee4] font-epilogue text-[#373737]">
       <div className="flex flex-col mb-10">
@@ -66,6 +129,12 @@ const Register = () => {
             className="w-60 h-10 pl-3"
           />
         </div>
+        {errors.firstname && (
+          <p className="text-[#eb5353] mb-1">{errors.firstname}</p>
+        )}
+        {errors.lastname && (
+          <p className="text-[#eb5353] mb-1">{errors.lastname}</p>
+        )}
         <p className="text-left mx-5 mb-1">
           Email<span className="text-[#eb5353]">&#42;</span>
         </p>
@@ -75,6 +144,8 @@ const Register = () => {
           className="w-auto mx-5 mb-5 h-10 pl-3"
           type="email"
         />
+        {errors.email && <p className="text-[#eb5353] mb-5">{errors.email}</p>}
+
         <p className="text-left mx-5 mb-1">
           Username<span className="text-[#eb5353]">&#42;</span>
         </p>
@@ -84,6 +155,8 @@ const Register = () => {
           className="w-auto mx-5 mb-5 h-10 pl-3"
           type="text"
         />
+        {errors.username && <p className="text-[#eb5353] mb-5">{errors.username}</p>}
+
         <p className="text-left mx-5 mb-1">
           Password<span className="text-[#eb5353]">&#42;</span>
         </p>
@@ -93,54 +166,23 @@ const Register = () => {
           className="w-auto mx-5 mb-5 h-10 pl-3"
           type="password"
         />
-        <p className="text-left mx-5 mb-1">
-          Role<span className="text-[#eb5353]">&#42;</span>
-        </p>
-        <div className="mb-5">
-          <div className="w-auto flex justify-around">
-            <div>
-              <input
-                type="radio"
-                id="volunteer"
-                name="role"
-                value="VOLUNTEER"
-                checked={selectedRole === "VOLUNTEER"}
-                onChange={(e) => setSelectedRole(e.target.value)}
-              />
-              <label className="ml-2" htmlFor="volunteer">
-                Volunteer
-              </label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="beneficiary"
-                name="role"
-                value="BENEFICIARY"
-                checked={selectedRole === "BENEFICIARY"}
-                onChange={(e) => setSelectedRole(e.target.value)}
-              />
-              <label className="ml-2" htmlFor="beneficiary">
-                Beneficiary
-              </label>
-            </div>
-          </div>
-        </div>
-        {selectedRole === "VOLUNTEER" && (
-          <>
-            <p className="text-left mx-5 mb-1">Bio</p>
-            <textarea
-              className="w-auto mx-5 mb-5 h-20 p-3"
-              rows="4"
-              cols="50"
-              onChange={(e) => setBio(e.target.value)}
-              placeholder="Let beneficiaries know a little bit about you."
-            />
-          </>
-        )}
+        {errors.password && <p className="text-[#eb5353] mb-5">{errors.password}</p>}
+
+        <select
+          value={selectedRole}
+          onChange={(e) => setSelectedRole(e.target.value)}
+          className="w-auto mx-5 mb-5 h-10 pl-3"
+        >
+          <option value="" disabled>
+            Select role
+          </option>
+          <option value="BENEFICIARY">beneficiary</option>
+          <option value="VOLUNTEER">volunteer</option>
+        </select>
+        {errors.selectedRole && <p className="text-[#eb5353] mb-5">{errors.selectedRole}</p>}
 
         <button
-          onClick={mutate}
+          onClick={handleSubmitBtn}
           className="hover:bg-[#4d7aff] bg-[#0753d8] text-white w-auto mx-5 text-s h-9 rounded-lg transition-colors duration-200 ease-in-out mb-8"
         >
           Register

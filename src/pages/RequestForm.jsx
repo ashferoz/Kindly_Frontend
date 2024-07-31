f4787f3946c0a09b94ab5a67c0ca092a1ef1809c
ashferoz <ash.safrina@gmail.com>
2024-07-31 21:15:32 +0800

    Request form constrain values are a drop down.

--- src/pages/RequestForm.jsx
+++ src/pages/RequestForm.jsx
@@ -3,6 +3,7 @@ import UserContext from "../contexts/user";
 import useFetch from "../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const RequestForm = () => {
   const userCtx = useContext(UserContext);
@@ -20,18 +21,44 @@ const RequestForm = () => {
       await usingFetch(
         "/api/requests",
         "PUT",
-        { beneficiary_uuid: userCtx.userUUID,
+        {
+          beneficiary_uuid: userCtx.userUUID,
           title,
           details,
           category,
           urgency,
-          location },
+          location,
+        },
         userCtx.accessToken
       ),
     onSuccess: () => {
       queryClient.invalidateQueries(["userRequests"]);
-      navigate(-1)
+      navigate(-1);
     },
+  });
+
+  const {
+    isSuccess: categorySuccess,
+    isError: categoryIsError,
+    error: categoryError,
+    isFetching: categoryIsFetching,
+    data: categorydData,
+  } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () =>
+      await usingFetch("/category/", "GET", undefined, undefined),
+  });
+
+  const {
+    isSuccess: locationSuccess,
+    isError: locationIsError,
+    error: locationError,
+    isFetching: locationIsFetching,
+    data: locationdData,
+  } = useQuery({
+    queryKey: ["locations"],
+    queryFn: async () =>
+      await usingFetch("/locations/", "GET", undefined, undefined),
   });
 
   return (
@@ -55,27 +82,50 @@ const RequestForm = () => {
           cols="50"
         />
 
-        <p className="text-left  mb-1">Category</p>
-        <input
+        <select
           value={category}
           onChange={(e) => setCategory(e.target.value)}
-          className="w-auto  mb-5 h-10 pl-3"
-          type="text"
-        />
-        <p className="text-left  mb-1">Urgency</p>
-        <input
+          className="w-auto mb-5 h-10 pl-3"
+        >
+          <option value="" disabled>
+            Select category
+          </option>
+          {categorySuccess &&
+            categorydData.map((item) => (
+              <option key={item.id} value={item.id}>
+                {item.id.toLowerCase().replace("_", " ")}
+              </option>
+            ))}
+        </select>
+
+        <select
           value={urgency}
           onChange={(e) => setUrgency(e.target.value)}
-          className="w-auto  mb-5 h-10 pl-3"
-          type="text"
-        />
-        <p className="text-left  mb-1">Location</p>
-        <input
+          className="w-auto mb-5 h-10 pl-3"
+        >
+          <option value="" disabled>
+            Select urgency
+          </option>
+          <option value="URGENT">urgent</option>
+          <option value="NOT_URGENT">not urgent</option>
+        </select>
+
+        <select
           value={location}
           onChange={(e) => setLocation(e.target.value)}
-          className="w-auto  mb-5 h-10 pl-3"
-          type="text"
-        />
+          className="w-auto mb-5 h-10 pl-3"
+        >
+          <option value="" disabled>
+            Select location
+          </option>
+          {locationSuccess &&
+            locationdData.map((item) => (
+              <option key={item.id} value={item.id}>
+                {item.id.toLowerCase().replace("_", " ")}
+              </option>
+            ))}
+        </select>
+
         <button
           onClick={mutate}
           className="bg-[#0753d8] text-white w-auto text-s h-9 rounded-lg hover:bg-[#eb5353] active:bg-[#a54040] transition-colors duration-150 ease-in-out"

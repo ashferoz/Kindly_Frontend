b9850be12ee3679e30dc404693a72403f75a36e5
ashferoz <ash.safrina@gmail.com>
2024-07-31 22:33:31 +0800

    Login use mutate and add validation.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import UserContext from "../contexts/user";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
@@ -11,33 +11,22 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const [submitError, setSubmitError] = useState("");
 
-
-  const { isError, error, data, refetch } = useQuery({
-    queryKey: ["login"],
-    queryFn: async () => {
-      try {
-        return await usingFetch("/auth/login", "POST", {
-          username,
-         password,
-        });
-      } catch (error) {
-        throw error.message;
-      }
-    },
-    enabled: false,
-  });
-
-  useEffect(() => {
-    if (data) {
+  const { mutate, isError, error, data } = useMutation({
+    mutationFn: async () =>
+      await usingFetch("/auth/login", "POST", { username, password }),
+    onSuccess: (data) => {
       userCtx.setAccessToken(data.access);
       const decoded = jwtDecode(data.access);
       userCtx.setRole(decoded.role);
-      userCtx.setUserUUID(decoded.uuid); 
-      navigate('/main');
-    }
-  }, [data]);
-  
+      userCtx.setUserUUID(decoded.uuid);
+      navigate("/main");
+    },
+    onError: (error) => {
+      setSubmitError("Username or password don't match.");
+    },
+  });
 
   return (
     <div className="flex items-center justify-center text-center min-h-screen bg-[#ffdee4] font-epilogue">
@@ -57,9 +46,12 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           className=" h-10 pl-3 mb-5"
         />
+
+        {submitError && <p className="text-[#eb5353] mb-3">{submitError}</p>}
+
         <button
-          onClick={refetch}
-          className="hover:bg-[#4d7aff] bg-[#0753d8] transition-colors duration-200 ease-in-out text-white w-auto text-s h-9 rounded-lg transition-colors duration-150 ease-in-out mb-5"
+          onClick={mutate}
+          className="hover:bg-[#4d7aff] bg-[#0753d8] transition-colors duration-200 ease-in-out text-white w-auto text-s h-9 rounded-lg mb-5"
         >
           Login
         </button>
